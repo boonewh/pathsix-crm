71ef6e09e43f63e64796f267f6c8a1c6665fb1ec
boonewh <boonewh@gmail.com>
2025-05-24 00:21:10 -0500

    Add email and password stuff and role assignment

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,2 +1,10 @@
 SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
 SECRET_KEY = "your-secret-key"
+
+MAIL_SERVER = "mail.gandi.net"
+MAIL_PORT = 587
+MAIL_USERNAME = "support@pathsixdesigns.com"
+MAIL_PASSWORD = "@Dr@g0ns2024"
+MAIL_USE_TLS = True
+MAIL_FROM_NAME = "PathSix CRM"
+MAIL_FROM_EMAIL = "support@pathsixdesigns.com"
